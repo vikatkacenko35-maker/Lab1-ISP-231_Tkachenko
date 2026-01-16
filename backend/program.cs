5dd99b099efbbd0ce776a5a5c6868a66d20a43cc
csgorun <dimitryi455@gmail.com>
2026-01-16 11:37:39 +0300

    ex 2 homework

--- backend/program.cs
+++ backend/program.cs
@@ -1,4 +1,20 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello!!");
+Console.WriteLine("Hello!! Enter your name");
+string name = Console.ReadLine();
+System.Console.WriteLine($"Hello {name}!");
 Console.WriteLine("Ткаченко Виктория Михайловна");
 Console.WriteLine(DateTime.Now);
+bool program_running = true;
+while (program_running){
+    Console.WriteLine(DateTime.Today);
+Console.WriteLine("Do you want to continue?");
+string answer = Console.ReadLine();
+switch (answer)
+{
+    case "y":
+            System.Console.WriteLine("Okay!");break;
+    case "n": System.Console.WriteLine("Bye!");
+            program_running = false;
+    break;
+}
+}
